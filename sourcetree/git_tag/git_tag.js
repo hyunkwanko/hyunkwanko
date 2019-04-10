1cfbe9bbd1a6c8b528883d4d5e4bcf3ef70bcca9
hyunkwanko <cjdshrwkd11@gmail.com>
2019-04-10 13:28:08 +0900

    Everyolo

---
+++ sourcetree/git_tag/git_tag.js
@@ -0,0 +1,20 @@
+// Particles
+particlesJS.load('particles-js', '../../static/particles.json', function() {
+    console.log('particles.js loaded - callback');
+});
+
+// Lodaing
+$(document).ready(function () {
+    $.fakeLoader({
+        bgColor: '#111',
+        spinner:"spinner7"
+    });
+});
+
+// Disqus
+(function() {
+    var d = document, s = d.createElement('script');
+    s.src = 'https://everyolo.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
