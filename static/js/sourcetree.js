2a83f3056d57d5431f85e2aff6fde7a29eb313eb
hyunkwanko <cjdshrwkd11@gmail.com>
2019-04-12 00:32:23 +0900

    Everyolo

--- static/js/sourcetree.js
+++ static/js/sourcetree.js
@@ -14,8 +14,8 @@ $(document).ready(function () {
 
 // Disqus
 var disqus_config = function () {
-    this.page.url = '{{https://hyunkwanko.github.io/sourcetree/}}';  // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = '{{hyunkwanko}}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    this.page.url = 'https://hyunkwanko.github.io/sourcetree/';  // Replace PAGE_URL with your page's canonical URL variable
+    // this.page.identifier = 'localhost'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
 };
 
 (function() { // DON'T EDIT BELOW THIS LINE
